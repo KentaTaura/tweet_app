de2a4ad720687c175e5985f1bb63c75ca37a20fd
Kenta Taura <taurakenta@gmail.com>
2021-05-27 23:18:16 +0900

    新規投稿機能実装 #5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,10 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
 
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'posts/index'
   get 'posts/new'
-  get 'posts/:id' => "posts#show"
+  get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
 
-  get '/' => "home#top"
-  get 'about' => "home#about"
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
   
 end
