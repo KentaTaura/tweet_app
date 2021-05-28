bb0682929d459e1d8f15b81fd67da592c6ebac69
Kenta Taura <taurakenta@gmail.com>
2021-05-28 10:02:37 +0900

    編集機能の実装 #8

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get 'posts/new'
   get 'posts/:id' => 'posts#show'
   post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
 
   get '/' => 'home#top'
   get 'about' => 'home#about'
-  
 end
